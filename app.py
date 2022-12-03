d6095672852dfc3290bf9eab44dd208cd7c02693
Chao Zhou <Czhou0k@gmail.com>
2022-12-03 12:31:09 -0500

    created a db

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db.init_app(app)
 
 @app.route("/")
 def hello_world():
     return render_template('base.html')
 
 if __name__ == "__main__":
+  with app.app_context():
+    db.create_all()
   app.run(debug=True)
