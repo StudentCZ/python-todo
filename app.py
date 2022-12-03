cac8f2143cce546c17c82290a72f791e3bcf88a8
Chao Zhou <Czhou0k@gmail.com>
2022-12-03 13:16:04 -0500

    removed old db and creating new

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(200))
+  name = db.Column(db.String(100))
   complete = db.Column(db.Boolean)
 
 @app.route("/")
-def hello_world():
-    return render_template('base.html')
+def home():
+    todo_list = Todo.query.all()
+    return render_template('base.html', todo_list=todo_list)
 
 if __name__ == "__main__":
   with app.app_context():
     db.create_all()
+
+    new_todo = Todo(title='hey', complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+
   app.run(debug=True)
